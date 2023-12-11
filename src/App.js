41cb7140751a87933bb35244590e43ec631faee6
Adriano <dada2011livia23@gmail.com>
2023-12-10 23:54:12 -0300

    ultimos ajustes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import CartolaInfo from './CartolaInfo'; // Importe o componente CartolaInfo
 
 const App = () => {
   return (
-    <div className="app-container">
-      {/* Conteúdo adicional ou outros componentes podem ser adicionados aqui */}
-      <CartolaInfo />
+    <div className="app-container"> 
+    <CartolaInfo />
     </div>
   );
 };
